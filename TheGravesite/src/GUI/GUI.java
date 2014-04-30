a99f8a4635c3313e7a0e0b6b0d3676e60c5c7195
IceHawk66 <bernd.mueller88@aol.de>
2014-04-30 10:59:27 +0200

    Erste GUI erstellt, bislang noch alles ohne Funktion

--- TheGravesite/src/GUI/GUI.java
+++ TheGravesite/src/GUI/GUI.java
@@ -1,5 +1,48 @@
 package GUI;
 
-public class GUI {
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 
+import javax.swing.*;
+
+public class GUI extends JFrame{
+	
+	public GUI(){
+		super();
+	}
+	
+	public GUI(int breite, int hoehe){
+		super("The Gravesite");
+		super.setSize(breite, hoehe);
+		
+		// Panels
+		JPanel buttons = new JPanel();
+		Dimension groessebuttons = new Dimension(breite / 2, hoehe / 2);
+		buttons.setPreferredSize(groessebuttons);
+		buttons.setMaximumSize(new Dimension(100, 100));
+		buttons.setMinimumSize(new Dimension(1,1));
+		
+		// Layouts
+		this.setLayout(new BorderLayout());
+		buttons.setLayout(new GridLayout(4,1));
+		
+		// Buttons
+		JButton starten = new JButton("Start");
+		JButton laden = new JButton("Laden");
+		JButton optionen = new JButton("Optionen");
+		JButton beenden = new JButton("Beenden");
+		
+		// Buttons dem Panel hinzufügen
+		buttons.add(starten);
+		buttons.add(laden);
+		buttons.add(optionen);
+		buttons.add(beenden);
+		
+		this.add(buttons, BorderLayout.CENTER);
+		
+		this.pack();
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		super.setVisible(true);
+	}
 }
--- TheGravesite/src/main/main.java
+++ TheGravesite/src/main/main.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.rmi.*;
+import GUI.*;
 import java.rmi.registry.*;
 
 public class main {
@@ -13,9 +14,13 @@ public class main {
 		String[][] spielerdaten;
 		String[][] map;
 		
+		// Arrays initialisieren mit Daten vom Server
 		spielerdaten = rawdata.getSpielerdaten();
 		map = rawdata.getMap();
 		
+		GUI hauptfenster = new GUI(1280, 860);
+		
+		// Testausgabe
 		for(int i = 0; i < map.length; i++){
 			for(int j = 0; j < map[0].length; j++){
 				System.out.print(map[i][j]);
