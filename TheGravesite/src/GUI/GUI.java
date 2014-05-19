5fd22ceae75f10c0ada30e53aa2460e064bbe81f
IceHawk66 <bernd.mueller88@aol.de>
2014-05-19 17:25:06 +0200

    GUI angepasst, vieles ausgelagert in eigene Methoden, kleine Bugfixes

--- TheGravesite/src/GUI/GUI.java
+++ TheGravesite/src/GUI/GUI.java
@@ -17,22 +17,47 @@ import java.net.URL;
 import javax.imageio.*;
 import javax.swing.*;
 
-public class GUI extends JFrame{
+public class GUI extends JFrame implements ActionListener{
 	private JLabel picLabel;
 	private String spielername;
 	private int[][] map;
 	
-	public GUI(){
-		super();
-	}
+	// Menuebar
+	JMenuBar menuBar;
+    JMenu fileMenu;
+	        
+	// Dropdown Eintraege
+	JMenuItem neuesSpiel;
+	JMenuItem ladeSpiel;
+	JMenuItem optionen;
+	JMenuItem speichern;
+	JMenuItem beendeSpiel;
+	
 	
 	public GUI(int breite, int hoehe, int[][] map){
 		super("The Gravesite");
 		super.setSize(breite, hoehe);
 		this.map = map;
 		
-
+		erstelleMenuBar();
+		ladeBild();
+		registriereListener();
 		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false);
+		super.setVisible(true);
+	}
+	
+	private void registriereListener() {
+		neuesSpiel.addActionListener(this);
+		ladeSpiel.addActionListener(this);
+		optionen.addActionListener(this);
+		speichern.addActionListener(this);
+		beendeSpiel.addActionListener(this);
+		
+	}
+
+	private void ladeBild() {
 		// Bild im Hauptmenue
 		BufferedImage hintergrund = null;
 		try {
@@ -44,88 +69,80 @@ public class GUI extends JFrame{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		// Menue hinzufuegen
-		JMenuBar menuBar = new JMenuBar();
-		
-		// Menues erstellen
-        JMenu fileMenu = new JMenu("Datei");
-        menuBar.add(fileMenu);
-        
-        // Dropdown Eintraege
-        JMenuItem neuesSpiel = new JMenuItem("Neues Spiel");
-        JMenuItem ladeSpiel = new JMenuItem("Spiel laden");
-        JMenuItem optionen = new JMenuItem("Optionen");
-        JMenuItem speichern = new JMenuItem("Speichern");
-        JMenuItem beendeSpiel = new JMenuItem("Beenden");
-        
-        // Eintraege dem Menue hinzufuegen
-        fileMenu.add(neuesSpiel);
-        fileMenu.add(ladeSpiel);
-        fileMenu.add(optionen);
-        fileMenu.add(speichern);
-        fileMenu.add(beendeSpiel);
-        
-        speichern.setEnabled(false);
-        
-        // MenuBar setzen
-        this.setJMenuBar(menuBar);
-        
-        // ActionListener fuer das Menue
-        neuesSpiel.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-            	spielername = JOptionPane.showInputDialog(null,"Geben Sie Ihren Namen ein","Neuen Spieler erstellen",JOptionPane.PLAIN_MESSAGE);
-            	picLabel.setVisible(false);
-            	zeichneOberflaeche();
-            }
-        });
-        
-        ladeSpiel.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-            	// ladeSpiel Dialog aufrufen
-            }
-        });
-        
-        optionen.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-            	// Optionen Menue
-            }
-        });
-        
-        speichern.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-            	// Spiel speichern
-            }
-        });
-        
-        beendeSpiel.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent arg0) {
-            	System.exit(0);
-            }
-        });
-        
-
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setResizable(false);
-		super.setVisible(true);
 	}
-	
+
+	private void erstelleMenuBar() {
+		// Menue hinzufuegen
+		menuBar = new JMenuBar();
+				
+		// Menues erstellen
+        fileMenu = new JMenu("Datei");
+        menuBar.add(fileMenu);
+		        
+		// Dropdown Eintraege
+		neuesSpiel = new JMenuItem("Neues Spiel");
+		ladeSpiel = new JMenuItem("Spiel laden");
+		optionen = new JMenuItem("Optionen");
+		speichern = new JMenuItem("Speichern");
+		beendeSpiel = new JMenuItem("Beenden");
+		        
+		// Eintraege dem Menue hinzufuegen
+		fileMenu.add(neuesSpiel);
+		fileMenu.add(ladeSpiel);
+		fileMenu.add(optionen);
+		fileMenu.add(speichern);
+		fileMenu.add(beendeSpiel);
+		        
+		speichern.setEnabled(false);
+		        
+		// MenuBar setzen
+		this.setJMenuBar(menuBar);
+	}
+
 	// Map abbilden
-	public void zeichneOberflaeche(){
+	public void ladeMap(){
 	    JPanel grid = new JPanel();
-	    grid.setLayout(new GridLayout(50, 50));
+	    grid.setLayout(new GridLayout(map.length, map[0].length));
 	    for (int i = 0; i < map.length; i++) {
 	        for (int j = 0; j < map[0].length; j++) {
 	        	if (map[i][j] == 2)
-                    grid.add(new JLabel("'"));
+                    grid.add(new JLabel("'")); // Boden
                 if (map[i][j] == 1)
-                    grid.add(new JLabel("#"));
+                    grid.add(new JLabel("#")); // Wand
                 if (map[i][j] == 0)
-                    grid.add(new JLabel(" "));
+                    grid.add(new JLabel(" ")); // Ausserhalb der Map
 	        }
 	    }
 	    this.add(grid);
 	    grid.setVisible(true);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String befehl = e.getActionCommand();
+
+		if (befehl.equals("Neues Spiel")) {
+			spielername = JOptionPane.showInputDialog(null,"Geben Sie Ihren Namen ein","Neuen Spieler erstellen",JOptionPane.PLAIN_MESSAGE);
+        	picLabel.setVisible(false);
+        	ladeMap();
+		}
+		
+		if(befehl.equals("Spiel laden")){
+			// ladeSpiel Dialog aufrufen
+		}
+		
+		if(befehl.equals("Optionen")){
+			// Optionen Menue
+		}
+		
+		if(befehl.equals("Spiel speichern")){
+			// Spiel speichern
+		}
+		
+		if(befehl.equals("Beenden")){
+			System.exit(0);
+		}
+        
 	}
 	
 }
--- TheGravesite/src/main/main.java
+++ TheGravesite/src/main/main.java
@@ -9,51 +9,25 @@ import java.rmi.registry.*;
 public class main {
 	
 	private static int[][] map;
+	private static String[][] spielerdaten;
 
 	public static void main(String[] args) throws RemoteException, NotBoundException {
 		try{
+			// Registry vom Server abrufen
 			Registry registry = LocateRegistry.getRegistry();
 			InterfaceRawData rawdata = (InterfaceRawData) registry.lookup("InterfaceRawData");
-			
-			String[][] spielerdaten;
-			
+
 			// Arrays initialisieren mit Daten vom Server
 			spielerdaten = rawdata.getSpielerdaten();
 			map = rawdata.getMap();
 			
 			GUI hauptfenster = new GUI(1280, 720, map);
-		
-			// Testausgabe
-			/*for(int i = 0; i < map.length; i++){
-				for(int j = 0; j < map[0].length; j++){
-					System.out.print(map[i][j]);
-				}
-				System.out.println();
-			}*/
-			
-			//darstellen(map);
 
 		} catch (ConnectException e){
 			System.err.println("Die Verbindung zum Server schlug fehl. Ist der Server gestartet?");
 		}
-		
 	}
-	
-    public static void darstellen(int[][] map) {
-        for (int i = 0; i < map.length; i++){
-            for (int j = 0; j < map[i].length; j++){
-                if (map[i][j] == 2)
-                    System.out.print("'");
-                if (map[i][j] == 1)
-                    System.out.print("#");
-                if (map[i][j] == 0)
-                    System.out.print(" ");
-                System.out.print(" ");
-            }
-            System.out.println("");
-        }
-    }
-    
+
     public static int[][] getMap(){
     	return map;
     }
